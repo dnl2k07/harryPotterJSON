7b1cdbdee4389e681ba1085bde44ebfa1811566b
Attila428 <pelei.attila.mark.404@dszcbaross.edu.hu>
2025-02-17 13:18:55 +0100

    változás a script.js-ben

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ async function fetchCharacters() {
     try {
         const response = await fetch('https://raw.githubusercontent.com/Laboratoria/LIM011-data-lovers/master/src/data/potter/potter.json');
         console.log(response);
+        const characters = await response.json()
+        console.log(characters);
     } catch (error) {
-        
+        console.error('Hiba történt az adatok lekérdezésekor: ', error)
     }
 }
